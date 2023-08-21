af5d6a701a76a828380efe3f261e14f65a7f64a9
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-08-21 16:00:11 -0500

    Add project files.

---
+++ Kata.CodeWars.AlphabetPosition/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+public static class Kata
+{
+    public static string AlphabetPosition(string text)
+    {
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        List<int> letters = new List<int>();
+
+        foreach (var letter in text.ToLower())
+        {
+            int index = alphabet.IndexOf(letter) + 1;
+            if (index > 0)
+                letters.Add(index);
+        }
+
+        return string.Join(" ", letters);
+    }
+}
